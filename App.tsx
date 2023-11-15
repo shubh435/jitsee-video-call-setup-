1e6bbba405f9fa90eca5999b5b79a5a88848f94b
shubham sarode <shubham.sarode@extwebtech.in>
2023-11-15 10:39:13 +0530

    completed the jitsee video call

--- App.tsx
+++ App.tsx
@@ -1,118 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+import React, {useState} from 'react';
+import {JitsiMeeting} from '@jitsi/react-native-sdk/index';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+function App() {
+  const [isTextInputClosed, setIsTextInputClosed] = useState(false);
+  const [value, setValue] = useState('');
+  const toggleRoom = () => {
+    setIsTextInputClosed(!isTextInputClosed);
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <>
+      {isTextInputClosed ? ( // @ts-ignore
+        <JitsiMeeting
+          eventListeners={
+            {
+              onReadyToClose: () => {
+                toggleRoom();
+              },
+            } as any
+          }
+          style={{flex: 1}}
+          room={value}
+          serverURL={'https://meet.jit.si/'}
+        />
+      ) : (
+        <View style={styles.container}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter room name"
+            value={value}
+            onChangeText={(text: string) => setValue(text)}
+          />
+          <Button title="join" onPress={() => toggleRoom()} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
+const styles = StyleSheet.create({
+  textInput: {
+    color: '#000',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '90%',
+    backgroundColor: '#fff',
+    padding: 10,
+    marginBottom: 15,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+  },
+});
